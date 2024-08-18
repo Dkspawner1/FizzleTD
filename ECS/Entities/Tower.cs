dea080cb120a1772a7fbd7ab80c4d1de990c4116
NickChrysanthou <NChrysanthou@myseneca.ca>
2024-08-18 13:25:58 -0400

    It works... sort of!

--- ECS/Entities/Tower.cs
+++ ECS/Entities/Tower.cs
@@ -8,7 +8,7 @@ namespace FizzleTD.ECS.Entities;
 public class Tower
 {
     // The Entity representing this Tower in the ECS framework.
-    protected Entity Entity { get; private set; }
+    public Entity Entity { get; private set; }
 
     private static readonly Dictionary<double, byte> AlphaDict = new Dictionary<double, byte>()
         {
@@ -26,14 +26,11 @@ public class Tower
         // Create a new entity within the provided world.
         Entity = world.CreateEntity();
 
-
         // Set the tower's position, considering the sprite's origin and scale
        towerComponent.Transform.Position = new Vector2(
             (Data.Window.Width - towerComponent.Sprite.TextureRegion.Width * towerComponent.Transform.Scale.X) / 2,
             (Data.Window.Height - towerComponent.Sprite.TextureRegion.Height * towerComponent.Transform.Scale.Y) / 2
         ) - towerComponent.Sprite.Origin * towerComponent.Transform.Scale;
-
-        towerComponent.CalculateTowerCenter();
 
         // Attach components to the entity
         Entity.Attach(towerComponent); // Attach the Tower itself as a component if needed
--- Scenes/GameScene.cs
+++ Scenes/GameScene.cs
@@ -3,6 +3,7 @@ using FizzleTD.ECS.Entities;
 using FizzleTD.ECS.Systems;
 using MonoGame.Extended;
 using MonoGame.Extended.ECS;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -27,11 +28,21 @@ public class GameScene : SceneBase
     public override void LoadContent(ContentManager Content)
     {
         var tower = new Tower(world, new TowerComponent(Content.Load<Texture2D>("textures/dorll")));
-        
-        var enemy0 = new EnemyComponent(Content.Load<Texture2D>("textures/dorll"), new Transform2());
+
+        var enemy0 = new EnemyComponent(Content.Load<Texture2D>("textures/clubs-1"), new Transform2(new Vector2(100f,20f)));
         var enemyEntity0 = new Enemy(world, enemy0);
 
-        List<EnemyComponent> wave0Enemies = [enemy0];
+        var enemy1 = new EnemyComponent(Content.Load<Texture2D>("textures/clubs-1"), new Transform2(new Vector2(1000f, 800f)));
+        var enemyEntity1 = new Enemy(world, enemy1);
+
+        var enemy2 = new EnemyComponent(Content.Load<Texture2D>("textures/clubs-1"), new Transform2(new Vector2(500, 900)));
+        var enemyEntity2 = new Enemy(world, enemy2);
+
+        enemy0.SetEnemyPath(tower.Entity);
+        enemy1.SetEnemyPath(tower.Entity);
+        enemy2.SetEnemyPath(tower.Entity);
+
+        List<EnemyComponent> wave0Enemies = [enemy0,enemy1,enemy2];
 
         var wave0 = new Wave(world, new WaveComponent(wave0Enemies));
     }
