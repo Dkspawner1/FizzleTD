f40b9b15ff58562102d015814b2339689d1a0f4f
NickChrysanthou <NChrysanthou@myseneca.ca>
2024-08-18 12:18:02 -0400

    Fixed positions and calculations

--- ECS/Components/TowerComponent.cs
+++ ECS/Components/TowerComponent.cs
@@ -4,7 +4,9 @@ using MonoGame.Extended.Graphics;
 namespace FizzleTD.ECS.Components;
 public class TowerComponent(Texture2D texture)
 {
-    public Sprite Sprite { get; private set; } = new(texture) { Origin = Vector2.Zero };
+    public Sprite Sprite { get => new(texture); }
     public Transform2 Transform { get; set; } = new Transform2(Vector2.Zero, 0f, Vector2.One);
-    public Vector2 TowerCenter;
+    public Vector2 TowerCenter => CalculateTowerCenter(); 
+
+    public Vector2 CalculateTowerCenter() => Transform.Position + (Sprite.TextureRegion.Bounds.Size.ToVector2() - Sprite.Origin) * Transform.Scale / 2;
 }
